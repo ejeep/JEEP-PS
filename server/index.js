e4241c5805a06d149cd5b8420b9140d156119aa5
Mukkun-cyber <akillerjam@gmail.com>
2024-12-16 00:59:42 +0800

    mongoAtlas

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const app = express();
 
 // Environment Variables
 const PORT = process.env.PORT || 3004;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/Jeep-PS';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://Jeep-PS:bdgVmQbzmPRn1FBr@jeepps.mwxa4.mongodb.net/?retryWrites=true&w=majority&appName=JeepPS';
 const SERVER_URL = process.env.SERVER_URL || `http://localhost:${PORT}`;
 // Middleware
 app.use(
